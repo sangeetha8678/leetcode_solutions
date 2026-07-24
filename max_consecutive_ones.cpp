ca81a98b5476e584297293153db9d0e2419dc87d
Sangeetha <sangeethaj872@gmail.com>
2026-07-24 22:39:35 +0530

    Added max consecutive ones

---
+++ max_consecutive_ones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0;
+        int ans = 0;
+
+        for (int num : nums) {
+
+            if (num == 1)
+                count++;
+            else
+                count = 0;
+
+            ans = max(ans, count);
+        }
+
+        return ans;
+    }
+};
