6162582ca819c68b2813afabc131372636f41770
Sangeetha <sangeethaj872@gmail.com>
2026-07-26 15:46:14 +0530

    Added subarray product less than k

---
+++ subarray_product_lessthan_k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k<=1)
+            return 0;
+        int l=0;
+        int p=1;
+        int a=0;
+        for(int r=0;r<nums.size();r++){
+            p *= nums[r];
+            while(p >= k){
+                p /= nums[l];
+                l++;
+            }
+            a +=(r-l+1);
+        }
+        return a;
+    }
+};
