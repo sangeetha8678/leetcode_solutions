5b4d929210ed59a72db7f18185381f0f6531f79e
Sangeetha <sangeethaj872@gmail.com>
2026-07-28 00:27:06 +0530

    Added subarray sum equal to k

---
+++ subarray_sum_divisible_by_k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        mp[0] = 1;
+        int prefix = 0;
+        int ans = 0;
+        for(int num : nums){
+            prefix += num;
+            int rem = prefix % k;
+            if(rem < 0)
+                rem += k;
+            ans += mp[rem];
+            mp[rem]++;
+        }
+        return ans;
+    }
+};
